354fd1313d288e06d56c17d4c3b791c17fcdd244
Saikot Hasan <skdm0111@gmail.com>
2024-08-30 01:40:39 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,4 +2,5 @@ export default function Footer() {
     return <footer className="py-5 bg-dark">
     <div className="container"><p className="m-0 text-center text-white">Copyright &copy; 2024</p></div>
     </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 }
